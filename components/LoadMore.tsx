b27e4c5cb47f8f1084276f00cbd98f179619277b
matinsekhavat <matinhyphen@gmail.com>
2024-06-09 19:53:59 +0330

    implement logic for load more file v1

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,13 +1,39 @@
 "use client";
+import { fetchAnime } from "@/app/action";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
+import AnimeCard, { AnimeProp } from "./AnimeCard";
+
+const PAGE = 2;
 
 function LoadMore() {
   const { ref, inView } = useInView();
+  const [animes, setAnimes] = useState<AnimeProp[]>([]);
+  const [pageIndex, setPageIndex] = useState(PAGE);
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(pageIndex)
+        .then((res) => {
+          setAnimes((prevAnimes) => [...prevAnimes, ...res]);
+          setPageIndex((prevPageIndex) => prevPageIndex + 1);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+      console.log(animes);
+    }
+  }, [inView]);
+
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {animes.map((item: AnimeProp, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
