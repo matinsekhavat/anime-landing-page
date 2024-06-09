6731011bb1f8a8f6b586891fdd9aa9e62da8d4aa
matinsekhavat <matinhyphen@gmail.com>
2024-06-09 19:40:27 +0330

    fix query params bug for page limit(gorget set equal)

--- app/action.ts
+++ app/action.ts
@@ -1,7 +1,7 @@
 "use server";
 export async function fetchAnime(page: number) {
   const res = await fetch(
-    `https://shikimori.one/api/animes/?page${page}&limit=8&order=popularity`
+    `https://shikimori.one/api/animes/?page=${page}&limit=8&order=popularity`
   );
   const data = await res.json();
   console.log("data is : ", data[0].image.original);
