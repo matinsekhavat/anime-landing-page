b09303d27c9156e36dfbd1e4a02f7a5dafb1e1ad
matinsekhavat <matinhyphen@gmail.com>
2024-06-09 18:30:57 +0330

    add actios _lib(services) file to fetch the animes

---
+++ app/action.ts
@@ -0,0 +1,9 @@
+"use server";
+export async function fetchAnime(page: number) {
+  const res = await fetch(
+    `https://shikimori.one/api/animes/?page${page}&limit=8&order=popularity`
+  );
+  const data = await res.json();
+  console.log("data is : ", data[0].image.original);
+  return data;
+}
